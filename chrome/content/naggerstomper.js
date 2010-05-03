6f3d9aa91380623e2203540f3c7c73fbfa4abd5e
Ryan Wolf <rwolf@borderstylo.com>
2010-05-02 23:17:12 -0700

    first working version with hardcoded regex and 1 global leak

--- chrome/content/naggerstomper.js
+++ chrome/content/naggerstomper.js
@@ -1,4 +1,26 @@
-(function () {
-  var ifsi = {};
-  Components.utils.import("resource://ifsi/ifsi.jsm", ifsi);
-})();
+var noscriptnag = /^http:\/\/noscript.net\/\?ver=.*&prev=.*/;
+
+window.addEventListener(
+  "load",
+  function () {
+    gBrowser.tabContainer.addEventListener(
+      "TabOpen",
+      function (event) {
+        var tab = event.target;
+        var browser = gBrowser.getBrowserForTab(tab);
+        browser.addEventListener(
+          "pageshow",
+          function (event) {
+            var window = event.originalTarget.defaultView;
+            if (window.location.href.match(noscriptnag)) {
+              gBrowser.removeTab(tab);
+            }
+          },
+          false
+        );
+      },
+      false
+    );
+  },
+  false
+);
