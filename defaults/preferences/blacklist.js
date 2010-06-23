ee6bd4e3bd544a0cc0b6fa1838afb09e9a2b6135
Ryan Wolf <rwolf@borderstylo.com>
2010-06-23 12:26:07 -0700

    added glass to blacklist

--- defaults/preferences/blacklist.js
+++ defaults/preferences/blacklist.js
@@ -1,4 +1,2 @@
-pref(
-  "extensions.updatemuffler.blacklist.noscript",
-  "^http:\/\/noscript.net\/\?ver=.*&prev=.*"
-);
+pref("extensions.updatemuffler.blacklist.noscript", "^http:\/\/noscript.net\/\?ver=.*&prev=.*");
+pref("extensions.updatemuffler.blacklist.glass", "^https:\/\/www.writeonglass.com\/#pages\/update-successful");
